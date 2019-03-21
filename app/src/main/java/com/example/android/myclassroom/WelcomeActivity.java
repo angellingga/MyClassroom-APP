f63b8a66006838a3857d36ed4eb9023d20dde04b
angellingga <angellingga98@gmail.com>
2019-03-21 13:20:45 +0700

    MyClassroom-APP-2 Welcoming Screen

--- app/src/main/java/com/example/android/myclassroom/WelcomeActivity.java
+++ app/src/main/java/com/example/android/myclassroom/WelcomeActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class WelcomeActivity extends AppCompatActivity {
+    //Perubahan
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
